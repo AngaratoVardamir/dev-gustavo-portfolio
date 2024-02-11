b8202f7a9c8780e0a926983cd90e83c361f4cc34
Gustavo Augusto <141056143+AngaratoVardamir@users.noreply.github.com>
2024-02-11 13:52:41 -0300

    Portifolio.V2
    Melhorando A Responsividade: [] [ ] [  ]

--- ativos/js/index.js
+++ ativos/js/index.js
@@ -1,9 +1,7 @@
+var Agora = new Date()
+var Hora = Agora.getHours()
+
 function Projeto(){
     var Em_Breve = window.alert('Em Breve')
 }
-function Copia(){
-    var texto = document.getElementById('Copia');
-    texto.select();
-    document.execCommand('copy');
-    alert('Texto copiado: ' + Copia.value);
-}
+Horas.innerHTML = `${Hora}`
