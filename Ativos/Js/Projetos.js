63d225e6716b9c544c3fd0848335f84517967296
Gustavo Augusto <141056143+AngaratoVardamir@users.noreply.github.com>
2024-02-17 20:43:43 -0300

    Portifolio(add Numero tell)

--- Ativos/Js/Projetos.js
+++ Ativos/Js/Projetos.js
@@ -1,3 +1,9 @@
 function Em_breve(){
     var em_breve = window.alert(`Em Breve!`)
 }
+function CopyNumero(){
+    var num = String('(31) 97240-7179')
+    navigator.clipboard.writeText(num).then(()=>{
+        alert('Numero Copiado')
+    })
+}
