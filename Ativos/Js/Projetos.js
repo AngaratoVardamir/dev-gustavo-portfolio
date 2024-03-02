26043bcaef598c8ae8d475fdd4e3e9863ab1c5ef
Gustavo Augusto <141056143+AngaratoVardamir@users.noreply.github.com>
2024-03-02 12:33:40 -0300

    Portifolio 6v
    Dando algumas Melhorias

--- Ativos/Js/Projetos.js
+++ Ativos/Js/Projetos.js
@@ -3,13 +3,36 @@ function Em_breve(){
 }
 function CopyNumero(){
     var num = String('(31) 97240-7179')
-    navigator.clipboard.writeText(num).then(()=>{
-        alert('Numero Copiado!!!')
-    })
+    navigator.clipboard.writeText(num)
 }
 function Didi(){
     var code = String('31ab7990-d68d-4163-a22e-2b8aec97a94c')
-    navigator.clipboard.writeText(code).then(()=>{
-        alert('QRcode Copiado!!!')
-    })
+    navigator.clipboard.writeText(code)
+}
+function CP_Segura(){
+    const Copiado = document.getElementById("Botão2");
+    Copiado.value = "COPIANDO"
+}
+function CP_Solta(){
+    const Copiado = document.getElementById("Botão2");
+    Copiado.value = "COPIA TEL"
+}
+
+function CV_Segura(){
+    const CV = document.getElementById("Botão1");
+    CV.value = "BAIXANDO"
+}
+function CV_Solta(){
+    const CV = document.getElementById("Botão1");
+    CV.value = "BAIXAR O CV"
+}
+
+
+function QR_Segura(){
+    const CV = document.getElementById("Apoio");
+    CV.innerHTML = "QRcode COPIANDO"
+}
+function QR_Solta(){
+    const CV = document.getElementById("Apoio");
+    CV.innerHTML = "Apoie Meu Trabalho"
 }
