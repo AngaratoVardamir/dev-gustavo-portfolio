e59955261b77023d657e2d0724181e3d6f20962a
Gustavo Augusto <141056143+AngaratoVardamir@users.noreply.github.com>
2024-03-21 16:20:56 -0300

    PortifólioV3.02

--- Js/Ativos.js
+++ Js/Ativos.js
@@ -1,20 +1,25 @@
 const Botoes = [...document.querySelectorAll(".Botão-da-Cabeça")];
-const BotãoCV = document.querySelector("#Botão1");
+const BotãoCV = document.querySelector("#Botão1 > a");
 const BotãoCN = document.querySelector("#Botão2");
 
 BotãoCV.addEventListener("click",()=>{
-    BotãoCV.innerHTML = (`<a download="Currículo-Gustavo.AuGusto" href="Assets/PDF/CV.pdf">A BAIXANDO O CV</a>`);
+    BotãoCV.innerHTML = (`BAIXANDO CV`);
     setTimeout(()=>{
-        BotãoCV.innerHTML = (`<a download="Currículo-Gustavo.AuGusto" href="Assets/PDF/CV.pdf">BAIXAR O CV</a>`);
+        BotãoCV.innerHTML = (`BAIXAR O CV`);
     },2000)
 });
 BotãoCN.addEventListener("click",()=>{
-    navigator.clipboard.writeText("+55 31 9 7240-7179").then()
+    navigator.clipboard.writeText("+55 31 9 7240-7179")
 
-    BotãoCN.innerHTML = ("NÚMERO COPIANDO");
+    BotãoCN.innerHTML = ("NÚMERO COPIADO");
     setTimeout(()=>{
         BotãoCN.innerHTML = ("COPIAR NÚMERO");
     },2000)
 });
 
 //=======================
+const QR_CODE = document.querySelector("#QR-CODE");
+
+QR_CODE.addEventListener("click",()=>{
+    navigator.clipboard.writeText("31ab7990-d68d-4163-a22e-2b8aec97a94c");
+})
