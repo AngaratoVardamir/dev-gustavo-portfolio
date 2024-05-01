c7769c89e93b6f0b71b8ba3a15aeb43bf018ff54
Gustavo Augusto <141056143+AngaratoVardamir@users.noreply.github.com>
2024-04-30 22:41:03 -0300

    Atualizando a Janela de Link PPT GAME

--- Js/Interanção2.js
+++ Js/Interanção2.js
@@ -2,7 +2,4 @@ const Janela_Proj = document.getElementById("Janela_Proj");
 const Link_Proj = document.getElementById("Link_Proj");
 const ProjDir = document.getElementById("ProjDir");
 const ProjEsq = document.getElementById("ProjEsq");
-
-Janela_Proj.addEventListener("click",()=>{
-    alert("Em Breve")
-})
+const Janela_Titulo = document.getElementById("Janela_Titulo");
