94100cb0f1858910871361ddc8b098b05c1e0076
Gustavo Augusto <141056143+AngaratoVardamir@users.noreply.github.com>
2024-05-04 14:30:07 -0300

    Adiconando Nova Janela Quick Munch

--- Js/Interanção2.js
+++ Js/Interanção2.js
@@ -3,3 +3,31 @@ const Link_Proj = document.getElementById("Link_Proj");
 const ProjDir = document.getElementById("ProjDir");
 const ProjEsq = document.getElementById("ProjEsq");
 const Janela_Titulo = document.getElementById("Janela_Titulo");
+
+//Listas
+let Nomes = ["PPT GAME","QM"]
+let Links = [`https://golden-taffy-e77184.netlify.app/`,`https://lanchonetequickmunch.netlify.app/`]
+let Imagens = ["url('Assets/Img/JogoPPT.png')","url('Assets/Img/QuickMunch.png')"]
+
+// Contador
+let Contador = 0
+
+console.log(Nomes.length)
+
+ProjDir.addEventListener("click",()=>{
+    if (Contador == 0){
+        Contador++
+        Link_Proj.href = Links[Contador];
+        Janela_Titulo.innerHTML = Nomes[Contador];
+        Janela_Proj.style.backgroundImage = (`${Imagens[Contador]}`);
+    }
+})
+
+ProjEsq.addEventListener("click",()=>{
+    if (Contador == 1){
+        Contador--
+        Link_Proj.href = Links[Contador];
+        Janela_Titulo.innerHTML = Nomes[Contador];
+        Janela_Proj.style.backgroundImage = (`${Imagens[Contador]}`);
+    }
+})
